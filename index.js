6c6add6e90933c3b107d161b767f0815437a1941
Morty <olaniran.abd@gmail.com>
2025-07-03 16:05:11 +0100

    feat: initialize the swagger dos

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const breezService = require('./services/breez.service');
 const invoiceRoutes = require('./routes/invoice.routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+
+// Setup Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Breez Invoice API',
+      version: '1.0.0',
+      description: 'An Express API to build your own self-custodial infrastucture using the Breez SDK nodeless implementation.',
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 3000}`,
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Use the invoice routes for all API endpoints
 app.use('/', invoiceRoutes);
